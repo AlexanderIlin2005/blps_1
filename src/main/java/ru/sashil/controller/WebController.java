6584884d2203375e80c63233fb4a90be88ef8191
AlexanderIlin2005 <sashailyin2005@gmail.com>
2026-03-03 20:41:13 +0300

    _

--- src/main/java/ru/sashil/controller/WebController.java
+++ src/main/java/ru/sashil/controller/WebController.java
@@ -8,21 +8,21 @@ public class WebController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index";  // Возвращает имя шаблона (index.html)
     }
 
     @GetMapping("/catalog")
     public String catalog() {
-        return "catalog";
+        return "catalog";  // Возвращает имя шаблона (catalog.html)
     }
 
     @GetMapping("/checkout")
     public String checkout() {
-        return "checkout";
+        return "checkout";  // Возвращает имя шаблона (checkout.html)
     }
 
     @GetMapping("/order/{orderNumber}")
     public String orderDetails() {
-        return "order-details";
+        return "order-details";  // Возвращает имя шаблона (order-details.html)
     }
 }
